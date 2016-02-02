945a97a53e0e783ecd5a2ea02864b8be2e9637fe
Anurag <anurag.choudhary93@gmail.com>
2016-02-02 08:36:15 -0600

    Added initial files

---
+++ js/smooth-scroll.js
@@ -0,0 +1,19 @@
+/**
+ * Created by an5ra on 2/2/2016.
+ */
+$(document).ready(function(){
+    $('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target
+                || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body')
+                    .animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
+});
